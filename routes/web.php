56d37f2410f5813bd9001c179e8dc46c228b6bb4
Adrian Leong <leongadrian36@gmail.com>
2022-08-23 22:07:50 +0800

    register step2

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,31 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+	use App\Http\Controllers\RegisterStepTwoController;
+	use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| Web Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider within a group which
+	| contains the "web" middleware group. Now create something great!
+	|
+	*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+	Route::get('/', function () {
+		return view('welcome');
+	});
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+	Route::group([ 'middleware' => [ 'auth', 'verified' ] ], function () {
+
+		Route::group([ 'middleware' => [ 'registration_completed' ] ], function () {
+			Route::get('/dashboard', function () {
+				return view('dashboard');
+			})->name('dashboard');
+		});
+
+		Route::get('/register-step-two', [ RegisterStepTwoController::class, 'create' ])->name('register-step2.create');
+		Route::post('/register-step-two', [ RegisterStepTwoController::class, 'store' ])->name('register-step2.post');
+	});
