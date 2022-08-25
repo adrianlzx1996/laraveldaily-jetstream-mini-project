5db5e01441eecd2e49a0cf22d06dfb18d14c3220
Adrian Leong <leongadrian36@gmail.com>
2022-08-25 21:40:26 +0800

    listing save/unsave button

---
+++ resources/views/livewire/listing-save-button.blade.php
@@ -0,0 +1,9 @@
+<div>
+	@if($savedListing)
+		<a href="#" wire:click.prevent="unsaveListing"
+		   class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Unsave</a>
+	@else
+		<a href="#" wire:click.prevent="saveListing"
+		   class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">Save</a>
+	@endif
+</div>
